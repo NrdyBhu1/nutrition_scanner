5547d3fa2da4acb214c065c2518b1358f8910c2f
Bhuvan Sankar Andela <NrdyBhu1@gmail.com>
2026-06-27 14:33:04 +0530

    Added icon and app name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.nutrition_scanner"
+    namespace = "com.nrdybhu1.nutrition_scanner"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.nutrition_scanner"
+        applicationId = "com.nrdybhu1.nutrition_scanner"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/nutrition_scanner/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.nutrition_scanner
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
